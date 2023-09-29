4ffd323d27f469dd8f5e227f03e8e9c5029864f8
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2023-09-30 01:59:49 +0530

    User Profile Changes

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -22,3 +22,30 @@ export const GET = async (request) => {
     });
   }
 };
+
+export const PATCH = async (request) => {
+  const { id, github, linkedin } = await request.json();
+
+  try {
+    await connectToDB();
+
+    if ((!github && !linkedin) || !id)
+      throw { status: 400, message: "No data provided" };
+
+    const user = await User.findById(id);
+    if (!user) throw { status: 404, message: "User not found" };
+
+    if (github) user.github = github;
+    if (linkedin) user.linkedin = linkedin;
+
+    await user.save();
+
+    return new Response(JSON.stringify({ message: "User updated" }), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: error.status || 500,
+    });
+  }
+};
--- app/user/[id]/page.jsx
+++ app/user/[id]/page.jsx
@@ -5,15 +5,71 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 
+const Modal = ({
+  setToggleModal,
+  textArea,
+  setTextArea,
+  handleEdit,
+  title,
+}) => {
+  return (
+    <div className={`fixed inset-0 flex items-center justify-center z-50`}>
+      <div
+        className="fixed inset-0 bg-black opacity-50 text-white"
+        onClick={() => setToggleModel(false)}
+      ></div>
+      <div className="bg-[#1f2937] p-4 rounded shadow-lg z-10">
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleEdit(textArea);
+          }}
+        >
+          <h2 className="text-xl font-semibold mb-4">Edit {title}</h2>
+          <textarea
+            className="w-full h-32 p-2 border rounded-md resize-none bg-black"
+            placeholder="Enter text here..."
+            value={textArea}
+            onChange={(e) => setTextArea(e.target.value)}
+          ></textarea>
+          <div className="mt-4 flex justify-between items-center">
+            <button
+              type="reset"
+              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+              onClick={() => setToggleModal(false)}
+            >
+              Close
+            </button>
+            <button
+              type="submit"
+              className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+            >
+              Edit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
 const Profile = ({ params }) => {
   const { data: session } = useSession();
   const { id } = params;
   const [profile, setProfile] = useState(null);
   const router = useRouter();
-  const [toggleEdit, setToggleEdit] = useState(false);
-  const [techStackInput, setTechStackInput] = useState("");
+  const [toggleModal, setToggleModal] = useState(false);
+  const [modalType, setModalType] = useState("");
+  const [textArea, setTextArea] = useState("");
 
-  const handleEdit = async (id, techStack) => {
+  const setModal = (value) => {
+    console.log(value);
+    setToggleModal(value);
+    setModalType("");
+  };
+
+  const handleEdit = async (techStack) => {
+    const id = profile?._id;
     const fetchOptions = {
       method: "POST",
       body: JSON.stringify({
@@ -25,13 +81,55 @@ const Profile = ({ params }) => {
     const response = await fetch("/api/techStack", fetchOptions);
     const data = await response.json();
 
-    setTechStackInput(techStack);
-    profile.techStack = techStack;
-
     if (response.status === 200) {
       toast.success(data.message);
-      setToggleEdit(false);
+      profile.techStack = techStack;
+      setModalType("");
     } else toast.error(data.message);
+
+    setToggleModal(false);
+  };
+
+  const handleEditGithub = async (github) => {
+    const id = profile?._id;
+    const fetchOptions = {
+      method: "PATCH",
+      body: JSON.stringify({
+        id,
+        github,
+      }),
+    };
+    const response = await fetch("/api/user", fetchOptions);
+    const data = await response.json();
+
+    if (response.status === 200) {
+      toast.success("Github Profile Updated");
+      profile.github = github;
+      setModalType("");
+    } else toast.error(data.message);
+
+    setToggleModal(false);
+  };
+
+  const handleEditLinkedin = async (linkedin) => {
+    const id = profile?._id;
+    const fetchOptions = {
+      method: "PATCH",
+      body: JSON.stringify({
+        id,
+        linkedin,
+      }),
+    };
+    const response = await fetch("/api/user", fetchOptions);
+    const data = await response.json();
+
+    if (response.status === 200) {
+      toast.success("LinkedIn Profile Updated");
+      profile.linkedin = linkedin;
+      setModalType("");
+    } else toast.error(data.message);
+
+    setToggleModal(false);
   };
 
   const fetchProfile = async () => {
@@ -46,7 +144,6 @@ const Profile = ({ params }) => {
       toast.error(data.message);
       router.push("/");
     }
-    setTechStackInput(data.techStack);
   };
   useEffect(() => {
     fetchProfile();
@@ -59,6 +156,69 @@ const Profile = ({ params }) => {
         </h1>
       ) : (
         <>
+          {session?.user?.email === profile?.email && (
+            <div className="flex gap-4 items-center flex-wrap">
+              <button
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                onClick={() => {
+                  setModalType("techStack");
+                  setToggleModal(true);
+                  setTextArea(profile.techStack);
+                }}
+              >
+                Edit Tech Stack
+              </button>
+              {modalType === "techStack" && toggleModal && (
+                <Modal
+                  setToggleModal={setModal}
+                  textArea={textArea}
+                  setTextArea={setTextArea}
+                  title="Tech Stack"
+                  handleEdit={handleEdit}
+                />
+              )}
+
+              <button
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                onClick={() => {
+                  setModalType("github");
+                  setToggleModal(true);
+                  setTextArea(profile.github);
+                }}
+              >
+                Edit Github Profile
+              </button>
+              {modalType === "github" && toggleModal && (
+                <Modal
+                  setToggleModal={setModal}
+                  textArea={textArea}
+                  setTextArea={setTextArea}
+                  title="Github Profile"
+                  handleEdit={handleEditGithub}
+                />
+              )}
+
+              <button
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                onClick={() => {
+                  setModalType("linkedin");
+                  setToggleModal(true);
+                  setTextArea(profile.linkedin);
+                }}
+              >
+                Edit LinkedIn Profile
+              </button>
+              {modalType === "linkedin" && toggleModal && (
+                <Modal
+                  setToggleModal={setModal}
+                  textArea={textArea}
+                  setTextArea={setTextArea}
+                  title="LinkedIn Profile"
+                  handleEdit={handleEditLinkedin}
+                />
+              )}
+            </div>
+          )}
           <h1 className="mt-5 mb-4 text-3xl font-extrabold leading-[1.15] sm:text-5xl">
             {profile.name}
           </h1>
@@ -82,74 +242,8 @@ const Profile = ({ params }) => {
           </h2>
           <h4 className="my-5 mb-2 text-xl font-semibold leading-[1.15] underline flex items-center">
             Tech Stack Details
-            {session?.user?.email === profile?.email && (
-              <p
-                className="text-white h-4 w-4 m-2 cursor-pointer"
-                onClick={() => setToggleEdit(true)}
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  data-name="Layer 1"
-                  viewBox="0 0 24 24"
-                  id="Edit"
-                >
-                  <path
-                    d="M3.5,24h15A3.51,3.51,0,0,0,22,20.487V12.95a1,1,0,0,0-2,0v7.537A1.508,1.508,0,0,1,18.5,22H3.5A1.508,1.508,0,0,1,2,20.487V5.513A1.508,1.508,0,0,1,3.5,4H11a1,1,0,0,0,0-2H3.5A3.51,3.51,0,0,0,0,5.513V20.487A3.51,3.51,0,0,0,3.5,24Z"
-                    fill="#ffffff"
-                    class="color000000 svgShape"
-                  ></path>
-                  <path
-                    d="M9.455,10.544l-.789,3.614a1,1,0,0,0,.271.921,1.038,1.038,0,0,0,.92.269l3.606-.791a1,1,0,0,0,.494-.271l9.114-9.114a3,3,0,0,0,0-4.243,3.07,3.07,0,0,0-4.242,0l-9.1,9.123A1,1,0,0,0,9.455,10.544Zm10.788-8.2a1.022,1.022,0,0,1,1.414,0,1.009,1.009,0,0,1,0,1.413l-.707.707L19.536,3.05Zm-8.9,8.914,6.774-6.791,1.4,1.407-6.777,6.793-1.795.394Z"
-                    fill="#ffffff"
-                    class="color000000 svgShape"
-                  ></path>
-                </svg>
-              </p>
-            )}
           </h4>
           <p className="mt-2 text-sm leading-[1.15]">{profile.techStack}</p>
-          {toggleEdit && (
-            <div
-              className={`fixed inset-0 flex items-center justify-center z-50`}
-            >
-              <div
-                className="fixed inset-0 bg-black opacity-50 text-white"
-                onClick={() => setToggleEdit(false)}
-              ></div>
-              <div className="bg-[#1f2937] p-4 rounded shadow-lg z-10">
-                <form
-                  onSubmit={(e) => {
-                    e.preventDefault();
-                    handleEdit(profile._id, techStackInput);
-                  }}
-                >
-                  <h2 className="text-xl font-semibold mb-4">
-                    Edit Tech Stack
-                  </h2>
-                  <textarea
-                    className="w-full h-32 p-2 border rounded-md resize-none bg-black"
-                    placeholder="Enter text here..."
-                    value={techStackInput}
-                    onChange={(e) => setTechStackInput(e.target.value)}
-                  ></textarea>
-                  <div className="mt-4 flex justify-between items-center">
-                    <button
-                      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                      onClick={() => setToggleEdit(false)}
-                    >
-                      Close
-                    </button>
-                    <button
-                      type="submit"
-                      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-                    >
-                      Edit
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          )}
           {profile.github && (
             <h3 className="mt-5 mb-2 text-sm font-semibold leading-[1.15] cursor-pointer text-blue-500">
               <a
