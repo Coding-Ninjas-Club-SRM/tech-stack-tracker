df0a3c363c49df3a267e6bbc4fadee11cc9b5008
Paavan Agrawal <33314332+AgrPaavan@users.noreply.github.com>
2023-09-29 15:15:52 +0530

    Make home protected

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,18 @@
+"use client";
+
 import Feed from "@components/Feed";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { data: session } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!session?.user) router.push("/api/auth/signin");
+  }, [session]);
   return (
     <div>
       <Feed />
